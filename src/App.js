0818c326000022dabb52e4a8141d36089136965f
Peter Song <psong562@gmnail.com>
2022-03-09 15:09:46 -0800

    created empty cubes for letters

--- src/App.js
+++ src/App.js
@@ -9,6 +9,33 @@ function App() {
   return (
     <div className="container">
 
+      <div className='heading'>
+        <div className="title">
+          WORDLE<sup>clone</sup>
+        </div>
+
+      </div>
+
+      <div className='cube'>
+        {[0, 1, 2, 3, 4, 5].map((row, rowIndex) => (
+          <div className='cubeRow'
+          key={rowIndex}>
+            {
+              [0, 1, 2, 3, 4].map((column, letterIndex) => (
+                <div className='letterBox'
+                 key={letterIndex}  >
+                </div>
+              ))
+            }
+          </div>
+        ))}
+      </div>
+
+
+
+
+
+
     <div className='keys'>
     <Keyboard />
     </div>
