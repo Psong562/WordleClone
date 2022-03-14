cef4ed6bdc58110a4b6ec157a08ed2f79cc1739d
Peter Song <psong562@gmnail.com>
2022-03-14 14:30:24 -0700

    added games

--- models/Game.js
+++ models/Game.js
@@ -4,47 +4,56 @@ const Game = new Schema({
   absentCharArray: [{
     type: String,
     maxlength: 5,
-    minlength: 1
+    minlength: 1,
+    required: false
   }],
   boardRowStatus: [{
     type: String,
     maxlength: 5,
-    minlength: 1
+    minlength: 1,
+    required: false
   }],
   absentCharArray: [{
     type: String,
     maxlength: 5,
-    minlength: 1
+    minlength: 1,
+    required: false
   }],
   boardWords: [{
     type: String,
     maxlength: 5,
-    minlength: 1
+    minlength: 1,
+    required: false
   }],
   correctCharArray: [{
     type: String,
     maxlength: 5,
-    minlength: 1
+    minlength: 1,
+    required: false
   }],
   presentCharArray: [{
     type: String,
     maxlength: 5,
-    minlength: 1
+    minlength: 1,
+    required: false
   }],
   presentCharArray: [{
     type: String,
     maxlength: 5,
-    minlength: 1
+    minlength: 1,
+    required: false
   }],
   rowIndex: [{
     type: Number,
     maxlength: 5,
-    minlength: 1
+    minlength: 1,
+    required: false
   }],
   solution: [{
     type: String,
     maxlength: 5,
-    minlength: 5
+    minlength: 5,
+    required: true
   }],
  
   user: {
